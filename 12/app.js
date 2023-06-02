38b2a3a046d0d8e27f37e3933e267e9dfa950623
Mary <maryandreeva100@gmail.com>
2023-06-02 17:09:17 +0300

    add 8-12

---
+++ 12/app.js
@@ -0,0 +1,29 @@
+/*
+Создайте объект calculator (калькулятор) с тремя методами:
+
+read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+sum() (суммировать) возвращает сумму сохранённых значений.
+mul() (умножить) перемножает сохранённые значения и возвращает результат.
+let calculator = {
+  // ... ваш код ...
+};
+*/
+
+let calculator = {
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt('a=', '');
+    this.b = +prompt('b=', '');
+  }
+}
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
