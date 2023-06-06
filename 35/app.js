1a5d7495322263add0d8afd2589474a14c22cd8f
Mary <maryandreeva100@gmail.com>
2023-06-06 14:33:27 +0300

    35-37

---
+++ 35/app.js
@@ -0,0 +1,25 @@
+/* 
+Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
+Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
+Если объект salaries пуст, то результат должен быть 0.
+*/
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+function sumSalaries(salaries) {
+  let sum = 0;
+  
+  if (Object.keys(salaries).length) {
+    for (let salary of Object.values(salaries)) {
+      sum += salary;
+    }
+  }
+
+  return sum;
+}
+
+console.log(sumSalaries(salaries)); // 650
---
+++ 37/app.js
@@ -0,0 +1,31 @@
+/* 
+У нас есть объект salaries с зарплатами.
+
+Создайте функцию topSalary(salaries), которая возвращает имя самого высокооплачиваемого сотрудника.
+
+Если объект salaries пустой, то нужно вернуть null.
+Если несколько высокооплачиваемых сотрудников, можно вернуть любого из них.
+P.S. Используйте Object.entries и деструктурирование, чтобы перебрать пары ключ/значение.
+*/
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+function topSalary(salaries) {
+  let max = 0;
+  let maxName = null;
+
+  for (const [name, salary] of Object.entries(salaries)) {
+    if (max < salary) {
+      max = salary;
+      maxName = name;
+    }
+  }
+
+  return maxName;
+};
+
+console.log(topSalary(salaries));
