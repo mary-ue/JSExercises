71d11c1d7293d20acc98a589d320f0d87e7bf04f
Mary <maryandreeva100@gmail.com>
2023-06-14 10:49:45 +0300

    65 (Generators)

---
+++ 65/app.js
@@ -0,0 +1,19 @@
+/* 
+Задачей является создать функцию-генератор pseudoRandom(seed), которая получает seed и создаёт генератор с указанной формулой.
+next = previous * 16807 % 2147483647
+*/
+
+function* pseudoRandom(num) {
+  let value = num;
+
+  while(true) {
+    value = value * 16807 % 2147483647;
+    yield value;
+  }
+}
+
+let generator = pseudoRandom(1);
+
+console.log(generator.next().value); // 16807
+console.log(generator.next().value); // 282475249
+console.log(generator.next().value); // 1622650073
