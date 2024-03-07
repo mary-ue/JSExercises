9c745b5517c5fbfcbc72e993b7a3002bdd8a7815
mary-ue <maryandreeva100@gmail.com>
2024-03-08 00:11:44 +0300

    135 (words order)

---
+++ 135/app.js
@@ -0,0 +1,30 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  if (words === '') {
+    return '';
+  }
+
+  const wordsArr = words.split(' ');
+  const orderedWordsArr = [];
+
+  for (let word of wordsArr) {
+    const num = parseInt(word.match(/\d+/));
+    orderedWordsArr[num - 1] = word;
+  }
+
+  return orderedWordsArr.join(' ');
+}
+
+console.log(order('is2 Thi1s T4est 3a'));
+console.log(order('4of Fo1r pe6ople g3ood th5e the2'));
+console.log(order(''));
