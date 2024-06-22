5343ffd6e03c9ba876c238490ae6b8a2ef28deed
mary-ue <maryandreeva100@gmail.com>
2024-06-23 00:11:37 +0300

    181 (open ul list)

---
+++ 181/app.js
@@ -0,0 +1,28 @@
+const tree = document.getElementById('tree');
+
+for (let li of tree.querySelectorAll('li')) {
+  let span = document.createElement('span');
+  span.classList.add('show');
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.onclick = function (event) {
+  console.log(event.target.tagName);
+  if (event.target.tagName != 'SPAN') return;
+
+  let childrenContainer = event.target.parentNode.querySelector('ul');
+  console.log(childrenContainer);
+
+  if (!childrenContainer) return;
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+
+  if (childrenContainer.hidden) {
+    event.target.classList.add('hide');
+    event.target.classList.remove('show');
+  } else {
+    event.target.classList.add('show');
+    event.target.classList.remove('hide');
+  }
+};
